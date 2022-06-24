a9af8f203c3c11864d5755429ab3b54e0c08a04f
gozooit <gozoo.it@protonmail.com>
2022-06-24 15:07:05 +0200

    build tree done + search data

---
+++ balanced_bst.rb
@@ -0,0 +1,60 @@
+class Node
+  attr_accessor :data, :left, :right
+
+  include Comparable
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+  def <=>(other)
+    data <=> other.data
+  end
+end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @root = build_tree(array.sort.uniq, 0, array.length - 1)
+  end
+
+  def build_tree(array, first, last)
+    return nil if first > last
+
+    mid = if (first + last).even?
+            (first + last) / 2
+          else
+            ((first + last - 1) / 2) + 1
+          end
+    node = Node.new(array[mid])
+    node.left = build_tree(array, first, mid - 1)
+    node.right = build_tree(array, mid + 1, last)
+    node
+  end
+
+  def print(node = @root)
+    return if node.nil?
+
+    puts node.data
+    print(node.left)
+    print(node.right)
+  end
+
+  def search(data, root = @root)
+    return root if root.nil? || root.data == data
+
+    root.data < data ? search(data, root.right) : search(data, root.left)
+  end
+
+  def insert(value)
+
+  end
+end
+
+# tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+tree = Tree.new([1, 2, 3, 4, 5, 6, 7])
+
+# p tree.print
+p tree.search(6)
